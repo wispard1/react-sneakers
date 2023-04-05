70a5610cce52b4b0af0c639d4a506ba0bd638d81
Anton <antonvlasov610@gmail.com>
2023-04-05 16:31:28 +0700

    Lesson 4

--- src/Components/CartBlock.js
+++ src/Components/CartBlock.js
@@ -1,10 +1,11 @@
-function CartBlock() {
+function CartBlock({ onCloseCart, items = [] }) {
   return (
-    <div style={{ display: 'none' }} className="cart-shadow">
-      <div style={{ display: 'none' }} className="cart-block">
+    <div className="cart-shadow">
+      <div className="cart-block">
         <h2 className="mb-40 d-flex justify-between">
           Корзина
           <img
+            onClick={onCloseCart}
             className="removeBtn cu-p"
             src="/img/btn-cart-st2.svg"
             alt="button"
@@ -12,43 +13,29 @@ function CartBlock() {
         </h2>
 
         <div className="items">
-          <div className="cartItem d-flex align-center justify-around ml-10">
-            <img
-              className="mr-20"
-              width={70}
-              height={70}
-              src="/img/sneakers/1.svg"
-              alt="sneakers"
-            />
-            <div>
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
+          {items.map((obj) => (
+            <div className="cartItem d-flex align-center justify-around ml-10">
+              <img
+                className="mr-20"
+                width={70}
+                height={70}
+                src={obj.imageUrl}
+                alt="sneakers"
+              />
+              <div>
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price} руб.</b>
+              </div>
+              <img
+              onClick={}
+                className="removeBtn"
+                src="/img/btn-cart-st2.svg"
+                alt="button"
+              />
             </div>
-            <img
-              className="removeBtn"
-              src="/img/btn-cart-st2.svg"
-              alt="button"
-            />
-          </div>
-          <div className="cartItem d-flex align-center justify-around ml-10">
-            <img
-              className="mr-20"
-              width={70}
-              height={70}
-              src="/img/sneakers/1.svg"
-              alt="sneakers"
-            />
-            <div>
-              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-              <b>12 999 руб.</b>
-            </div>
-            <img
-              className="removeBtn"
-              src="/img/btn-cart-st2.svg"
-              alt="button"
-            />
-          </div>
+          ))}
         </div>
+
         <div className="cartTotalBlock">
           <ul>
             <li>
